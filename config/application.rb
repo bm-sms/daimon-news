1b3d112aea6a05a79cc023588edebf713a46b1d1
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-10-26 10:11:17 +0900

    Don't generate helper & assets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Pochi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.app_generators do |g|
+      g.helper false
+      g.assets false
+    end
   end
 end
