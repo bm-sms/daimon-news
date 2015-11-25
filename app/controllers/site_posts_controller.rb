bb59dcf0ceb641f10855c26331d06bdb698d4657
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-11-25 14:04:58 +0900

    Extract a before_action to setup site

--- app/controllers/site_posts_controller.rb
+++ app/controllers/site_posts_controller.rb
@@ -1,16 +1,20 @@
 class SitePostsController < ApplicationController
+  before_action :setup_site
   layout :layout_name
 
   def index
-    @site = Site.find(params[:site_id])
     @posts = @site.posts
   end
 
   def show
-    @post = Site.find(params[:site_id]).posts.find(params[:id])
+    @post = @site.posts.find(params[:id])
   end
 
   private
+  def setup_site
+    @site = Site.find(params[:site_id])
+  end
+
   def layout_name
     "site#{@site.id}"
   end
