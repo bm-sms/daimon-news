326d058125acf6edb77738a5882817f778643157
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-02 17:15:51 +0900

    Fix a typo...

--- daimon_news_admin/db/migrate/20151202064313_create_daimon_news_admin_posts.rb
+++ daimon_news_admin/db/migrate/20151202064313_create_daimon_news_admin_posts.rb
@@ -2,7 +2,7 @@ class CreateDaimonNewsAdminPosts < ActiveRecord::Migration
   def change
     create_table :daimon_news_admin_posts do |t|
       t.string :title
-      t.test :body
+      t.text :body
       t.references :site, index: true, foreign_key: true
 
       t.timestamps null: false
