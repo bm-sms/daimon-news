6efa370748fa8a9de26c8d43ab4bda07fc0b9457
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-02 16:06:18 +0900

    Rremove needless models

--- app/models/site.rb
+++
@@ -1,3 +0,0 @@
-class Site < ActiveRecord::Base
-  has_many :posts
-end
--- db/migrate/20151026011256_create_sites.rb
+++
@@ -1,9 +0,0 @@
-class CreateSites < ActiveRecord::Migration
-  def change
-    create_table :sites do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
