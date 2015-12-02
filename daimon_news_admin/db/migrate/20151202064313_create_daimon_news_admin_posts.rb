e97085fa502b82c14a2229e67d664b5a683ee78c
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-02 17:16:33 +0900

    Add NOT NULL constraint

--- daimon_news_admin/db/migrate/20151202064313_create_daimon_news_admin_posts.rb
+++ daimon_news_admin/db/migrate/20151202064313_create_daimon_news_admin_posts.rb
@@ -3,7 +3,7 @@ class CreateDaimonNewsAdminPosts < ActiveRecord::Migration
     create_table :daimon_news_admin_posts do |t|
       t.string :title
       t.text :body
-      t.references :daimon_news_admin_site, index: true, foreign_key: true
+      t.references :daimon_news_admin_site, index: true, foreign_key: true, null: false
 
       t.timestamps null: false
     end
