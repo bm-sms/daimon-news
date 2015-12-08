8ef5f63c09f257ab0025bbae9004a6cb35b35e36
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-08 14:47:14 +0900

    Revert "Mount DaimonNewsBlog to /blog for now"
    This reverts commit c40997d29aa96baec5912d86b84381c9a6e11352.

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem 'rabl'
 gem 'sass-rails'
 gem 'uglifier'
 gem 'daimon_news_admin', path: './daimon_news_admin'
-gem 'daimon_news_blog', path: './daimon_news_blog'
 
 group :development, :test do
   gem 'byebug'
--- daimon_news_blog/config/routes.rb
+++ daimon_news_blog/config/routes.rb
@@ -1,6 +1,2 @@
 DaimonNewsBlog::Engine.routes.draw do
-  resources :sites do
-    resources :posts, only: %i(index show), controller: :site_posts
-  end
-  match '/', via: :get, controller: :site_posts, action: :index
 end
