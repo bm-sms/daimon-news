6c4c54a9a3d92b1185e1a363afc91311c9725165
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-08 14:47:51 +0900

    Revert "admin: Use foreign_key to simplify"
    This reverts commit b2e9cbf0c080ddebb132e1727f7b3fc7f0f33917.

--- daimon_news_admin/app/models/daimon_news_admin/post.rb
+++ daimon_news_admin/app/models/daimon_news_admin/post.rb
@@ -1,5 +1,5 @@
 module DaimonNewsAdmin
   class Post < ActiveRecord::Base
-    belongs_to :site, class_name: "::DaimonNewsAdmin::Site", foreign_key: 'daimon_news_admin_site_id'
+    belongs_to :daimon_news_admin_site, class_name: "::DaimonNewsAdmin::Site"
   end
 end
--- daimon_news_admin/app/models/daimon_news_admin/site.rb
+++ daimon_news_admin/app/models/daimon_news_admin/site.rb
@@ -1,5 +1,5 @@
 module DaimonNewsAdmin
   class Site < ActiveRecord::Base
-    has_many :posts, class_name: "::DaimonNewsAdmin::Post", foreign_key: 'daimon_news_admin_site_id'
+    has_many :daimon_news_admin_posts, class_name: "::DaimonNewsAdmin::Post"
   end
 end
