463be68d2f790f0a8d7f910017d1eff078ac437b
Kenji Okimoto <okimoto@clear-code.com>
2015-12-09 16:22:38 +0900

    Permit :site_id

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -54,6 +54,6 @@ class Admin::CategoriesController < Admin::ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def category_params
-    params.require(:category).permit(:name, :description)
+    params.require(:category).permit(:name, :description, :site_id)
   end
 end
