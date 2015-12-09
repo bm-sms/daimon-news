29af0dd2799c35fde8e830fd1d60257d3704f378
Kenji Okimoto <okimoto@clear-code.com>
2015-12-09 15:07:40 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209023430) do
+ActiveRecord::Schema.define(version: 20151209060628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151209023430) do
     t.datetime "updated_at",   null: false
     t.datetime "published_at"
     t.boolean  "published"
+    t.integer  "category_id"
   end
 
   add_index "posts", ["site_id"], name: "index_posts_on_site_id", using: :btree
