8266f5726659fac39dc51c67bc23f56d7596d060
Kenji Okimoto <okimoto@clear-code.com>
2015-12-09 15:58:55 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209060628) do
+ActiveRecord::Schema.define(version: 20151209061323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151209060628) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "site_id"
   end
 
   create_table "posts", force: :cascade do |t|
