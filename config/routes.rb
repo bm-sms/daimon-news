71267aa49d8b7494062bfd980c4367705b06c592
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-10 15:06:16 +0900

    Remove admin route

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  namespace :admin do
-    root 'root#index'
-
-    resources :sites
-    resources :posts
-    resources :categories
-  end
-
   concern :site do
     resources :posts, only: %i(index show)
     resources :categories, only: %i(show), path: 'category'
