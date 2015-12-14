8cf6c16b1bc5d4a531d02c6353ac78766b15d4a6
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-14 16:13:07 +0900

    Fix broken fixed_pages#show
    fixed pages don't have any categories

--- app/controllers/fixed_pages_controller.rb
+++ app/controllers/fixed_pages_controller.rb
@@ -2,8 +2,6 @@ class FixedPagesController < ApplicationController
   before_action :setup_site
 
   def show
-    @post = @site.fixed_pages.find_by!(slug: params[:id])
-
-    render 'posts/show'
+    @fixed_page = @site.fixed_pages.find_by!(slug: params[:id])
   end
 end
