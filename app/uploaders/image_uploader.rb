02b0ec0ed375d86f2644c385675c915c777fdfee
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-14 14:59:05 +0900

    Upload to S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if fog_credentials.present?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
