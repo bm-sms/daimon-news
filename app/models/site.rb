0375c73299bf1b49b52fe30f45cf7d537be92ca1
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-17 17:20:39 +0900

    Add missing association

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,4 +3,5 @@ class Site < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :fixed_pages, dependent: :destroy
   has_many :topics, dependent: :destroy
+  has_many :meta_tags, dependent: :destroy
 end
