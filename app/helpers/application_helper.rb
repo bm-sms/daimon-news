f9792feb7503d43e4939533bb4410a5d5665b166
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-18 16:35:30 +0900

    Show post title in title bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,9 @@ module ApplicationHelper
     if @page_meta_information.try(:title)
       title = "#{@page_meta_information.title} | #{title}"
     end
+    if @post.try(:title)
+      title = "#{@post.title} | #{title}"
+    end
     title
   end
 end
