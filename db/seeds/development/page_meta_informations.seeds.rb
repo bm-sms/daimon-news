a6ffdb012ee919a22f79213c9983acbfe5409111
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-18 16:11:29 +0900

    Add a seed for about page

--- db/seeds/development/page_meta_informations.seeds.rb
+++ db/seeds/development/page_meta_informations.seeds.rb
@@ -6,6 +6,11 @@ after 'development:sites' do
     description: 'description of site1'
   )
   site1.page_meta_informations.create!(
+    path:        '/about',
+    title:       'About site1',
+    description: "What's site1?"
+  )
+  site1.page_meta_informations.create!(
     path:        '/policy',
     title:       'Privacy Policy',
     description: 'This is the privacy policy page.',
