7167a9e7bb281653bf8537dc72ab27188c84e6dc
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-12-18 17:45:24 +0900

    Fix indent

--- app/admin/page_meta_information.rb
+++ app/admin/page_meta_information.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register PageMetaInformation do
-permit_params :path, :title, :description, :keywords, :noindex, :site_id
+  permit_params :path, :title, :description, :keywords, :noindex, :site_id
 end
