f83926b5fe335fb427cf4c69dafe34e7aa6025f0
Kenji Okimoto <okimoto@clear-code.com>
2015-12-18 17:45:53 +0900

    Arrange policy
    We need more work to control authorization.

--- app/policies/active_admin/application_policy.rb
+++ app/policies/active_admin/application_policy.rb
@@ -3,5 +3,29 @@ module ActiveAdmin
     def index?
       @user.admin? || @user.site_owner?
     end
+
+    def show?
+      @user.admin? || @user.site_owner?
+    end
+
+    def create?
+      @user.admin? || @user.site_owner?
+    end
+
+    def new?
+      create?
+    end
+
+    def update?
+      @user.admin? || @user.site_owner?
+    end
+
+    def edit?
+      @user.admin? || @user.site_owner?
+    end
+
+    def destroy?
+      @user.admin? || @user.site_owner?
+    end
   end
 end
