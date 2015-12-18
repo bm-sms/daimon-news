888a9427be95e03c2dd2be9d8e0ce916874fea21
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-18 16:02:23 +0900

    Topics#index no longer used

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,5 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by!(slug: params[:id])
     @posts = current_site.posts.where(category: @category).published
-
-    render 'posts/index'
   end
 end
