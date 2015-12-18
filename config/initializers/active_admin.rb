02c92c39da8ce767f9b6663de112d8921053b239
Kenji Okimoto <okimoto@clear-code.com>
2015-12-18 15:17:53 +0900

    Add pundit configuration

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -63,11 +63,13 @@ ActiveAdmin.setup do |config|
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
   # config.authorization_adapter = ActiveAdmin::CanCanAdapter
+  config.authorization_adapter = ActiveAdmin::PunditAdapter
 
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
   # case when Pundit is unable to find suitable policy.
   # config.pundit_default_policy = "MyDefaultPunditPolicy"
+  config.pundit_default_policy = "::ActiveAdmin::ApplicationPolicy"
 
   # You can customize your CanCan Ability class name here.
   # config.cancan_ability_class = "Ability"
