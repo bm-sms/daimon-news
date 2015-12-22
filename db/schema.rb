1c85782e00ea4819a6355675ac6481cd8c40bce9
okkez <okkez000@gmail.com>
2015-12-22 16:20:04 +0900

    Add favicon_url to Site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222064556) do
+ActiveRecord::Schema.define(version: 20151222071746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20151222064556) do
     t.string   "copyright"
     t.boolean  "bbs_enabled", default: false,         null: false
     t.string   "logo_url"
+    t.string   "favicon_url"
   end
 
   create_table "topics", force: :cascade do |t|
