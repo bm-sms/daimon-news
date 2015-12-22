c059dbe715ef21b8f26585d8b3135f47c0faaa7a
okkez <okkez000@gmail.com>
2015-12-22 13:27:45 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -102,19 +102,19 @@ ActiveRecord::Schema.define(version: 20151221174319) do
   add_index "topics", ["site_id"], name: "index_topics_on_site_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "role",                   default: 2,  null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.string   "role",                   default: "user", null: false
     t.string   "name"
   end
 
