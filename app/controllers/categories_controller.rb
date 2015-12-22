ecdc8e27ab13711b29fe62f42db6dd50a22b1d8a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-22 19:11:38 +0900

    :innocent:

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @category = Category.find_by!(slug: params[:id])
+    @category = current_site.categories.find_by!(slug: params[:id])
     @posts = current_site.posts.where(category: @category).published.order_by_recently.page(params[:page])
   end
 end
