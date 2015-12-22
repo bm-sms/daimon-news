8953b9d8a731beafda11e2581da498b007233c96
okkez <okkez000@gmail.com>
2015-12-22 16:53:56 +0900

    Break a long line

--- app/admin/site.rb
+++ app/admin/site.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Site do
-  permit_params :name, :js_url, :css_url, :fqdn, :tagline, :copyright, :bbs_enabled, :logo_url
+  permit_params(:name,
+                :js_url,
+                :css_url,
+                :fqdn,
+                :tagline,
+                :copyright,
+                :bbs_enabled,
+                :logo_url)
 end
