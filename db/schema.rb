ef7e57c3280ed1970c2737a8311ab12a1981b01b
okkez <okkez000@gmail.com>
2015-12-22 17:54:07 +0900

    Add gtm_id to Site
    gtm_id is a ID for Google Tag Manager.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222073245) do
+ActiveRecord::Schema.define(version: 20151222080641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20151222073245) do
     t.string   "logo_url"
     t.string   "favicon_url"
     t.string   "mobile_favicon_url"
+    t.string   "gtm_id"
   end
 
   create_table "topics", force: :cascade do |t|
