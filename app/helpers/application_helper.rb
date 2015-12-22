76a7fb5796a19e811b9f7f6e2ed4f2eebe882e23
okkez <okkez000@gmail.com>
2015-12-22 17:55:23 +0900

    Add helper for Google Tag Manager

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     markdown.render(markdown_text)
   end
+
+  def google_tag_manager(gtm_id)
+    if gtm_id
+      render partial: 'google_tag_manager', locals: { gtm_id: gtm_id }
+    end
+  end
 end
