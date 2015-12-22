a8ef32b6c59e1648a247a3683248de86b50d154b
okkez <okkez000@gmail.com>
2015-12-22 17:54:42 +0900

    Permit gtm_id and make gtm_id editable

--- app/admin/site.rb
+++ app/admin/site.rb
@@ -8,5 +8,22 @@ ActiveAdmin.register Site do
                 :bbs_enabled,
                 :logo_url,
                 :favicon_url,
-                :mobile_favicon_url)
+                :mobile_favicon_url,
+                :gtm_id)
+
+  form do |f|
+    f.semantic_errors
+    f.inputs(:name,
+             :js_url,
+             :css_url,
+             :fqdn,
+             :tagline,
+             :copyright,
+             :bbs_enabled,
+             :logo_url,
+             :favicon_url,
+             :mobile_favicon_url,
+             :gtm_id)
+    f.actions
+  end
 end
