17d2e2ff4515d13efc4e55962596f33539eb6686
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-22 16:14:17 +0900

    Permit :logo_url

--- app/admin/site.rb
+++ app/admin/site.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Site do
-  permit_params :name, :js_url, :css_url, :fqdn, :tagline, :copyright, :bbs_enabled
+  permit_params :name, :js_url, :css_url, :fqdn, :tagline, :copyright, :bbs_enabled, :logo_url
 end
