a1abc191a763ebfc3f9a495c45a5155dbc0ed483
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2015-12-24 13:52:07 +0900

    Canonical to view-all page for SEO

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def show
     @post = current_site.posts.published.find(params[:id])
-    @pages = Kaminari.paginate_array(@post.pages).page(params[:page]).per(1)
+
+    @pages =
+      if params[:all]
+        @post.pages
+      else
+        Kaminari.paginate_array(@post.pages).page(params[:page]).per(1)
+      end
   end
 end
