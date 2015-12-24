b0d36130e1f6cf398051d6397fc5bd3db695e78c
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-12-24 16:23:16 +0900

    Add og:* and article:* tags for post

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,2 +1,21 @@
 module PostDecorator
+  def to_og_params
+    {
+      locale: 'ja_JP',
+      type: 'article',
+      title: title,
+      description: body,
+      url: post_url(self, all: true),
+      site_name: site.name,
+      modified_time: updated_at.to_datetime.to_s,
+      image: thumbnail_url
+    }
+  end
+
+  def to_article_params
+    {
+      section: category.name,
+      published_time: published_at.to_datetime.to_s
+    }
+  end
 end
