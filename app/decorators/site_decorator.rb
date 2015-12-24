81ee33b8a282ebfb6863c3f3dec7313bc9a20e7d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-12-24 16:43:36 +0900

    Add og:* for site

---
+++ app/decorators/site_decorator.rb
@@ -0,0 +1,13 @@
+module SiteDecorator
+  def to_og_params
+    {
+      locale: 'ja_JP',
+      type: 'website',
+      title: [name, tagline].select(&:present?).join(' | '),
+      description: description,
+      url: root_url,
+      site_name: name,
+      image: logo_url
+    }
+  end
+end
