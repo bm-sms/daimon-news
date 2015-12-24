b4450340d411efef5924384e8d9b83320a22dbfd
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-12-24 16:12:38 +0900

    Remove unused

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -1,14 +1,2 @@
 module PostDecorator
-  include ActionView::Helpers::TagHelper
-
-  def as_html
-    content_tag 'article' do
-      html_title = content_tag 'h2' do
-        title
-      end
-      html_body = simple_format body
-
-      [html_title, html_body].join.html_safe
-    end
-  end
 end
