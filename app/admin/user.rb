0c6c4da8001eebe80009e2266325538f13c5712c
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-05 17:47:04 +0900

    Fix user form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register User do
   permit_params :name, :email, :password, :role
 
+  form do |f|
+    f.semantic_errors
+    f.inputs :name, :email, :password, :role
+    f.actions
+  end
+
   index do
     column :name
     column :email
