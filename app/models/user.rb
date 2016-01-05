3c8d3fff077eb5003434829936335b51abc6abb0
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-05 19:08:43 +0900

    Remove registerable, recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ActiveRecord::Base
   enum role: { admin: "admin", site_owner: "site_owner", user: "user" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
