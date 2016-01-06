2113c47bbc6a7289b03dfd48a7539f607d47611e
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-06 12:50:34 +0900

    Remove users.role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-  enum role: { admin: "admin", site_owner: "site_owner", user: "user" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
