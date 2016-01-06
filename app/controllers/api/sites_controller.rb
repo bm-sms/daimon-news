95fa47cbcf28c86cadfba95b031d2d6f8e852792
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-06 11:56:52 +0900

    Remove API to update {js,css}_url
    Now this is unused.
    Close https://github.com/bm-sms/daimon-news-multi-tenant/issues/132

--- app/controllers/api/sites_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::SitesController < ApplicationController
-  protect_from_forgery with: :null_session
-
-  def update
-    Site.find(params[:id]).update!(params.require(:site).permit(:js_url, :css_url))
-
-    head :ok
-  end
-end
