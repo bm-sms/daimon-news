85b36a64fab1093a0113a96213fa01c9752d25b7
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-06 18:02:15 +0900

    Pretty seeds

--- db/seeds/development/sites.seeds.rb
+++ db/seeds/development/sites.seeds.rb
@@ -1,12 +1,12 @@
 Site.create!(
   name: 'site1',
   fqdn: ENV['HEROKU_APP_NAME'].present? ? "#{ENV['HEROKU_APP_NAME']}.herokuapp.com" : 'localhost',
-  js_url:  'https://rawgit.com/bm-sms/daimon-news-multi-tenant/a7a19c9d984df9ca4d2d481e537cfd81db8c3a5d/site.js',
-  css_url: 'https://rawgit.com/bm-sms/daimon-news-multi-tenant/a7a19c9d984df9ca4d2d481e537cfd81db8c3a5d/site.css'
+  js_url:  'themes/default/application',
+  css_url: 'themes/default/application'
 )
 
 Site.create!(
   name:    'site2',
-  js_url:  'https://rawgit.com/myokoym/public-site1/master/site.js',
-  css_url: 'https://rawgit.com/myokoym/public-site1/master/site.css'
+  js_url:  'themes/default/application',
+  css_url: 'themes/default/application'
 )
