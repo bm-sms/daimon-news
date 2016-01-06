e246baa7e472d9bf93aeeb759432b068aabb3e9f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-06 15:26:38 +0900

    Specify attrs on form for gtm_id

--- app/admin/site.rb
+++ app/admin/site.rb
@@ -1,25 +1,34 @@
 ActiveAdmin.register Site do
-  permit_params(
-    :name,
-    :js_url,
-    :css_url,
-    :fqdn,
-    :tagline,
-    :description,
-    :logo_url,
-    :favicon_url,
-    :mobile_favicon_url,
-    :gtm_id,
-    :content_header_url,
-    :promotion_url,
-    :sns_share_caption,
-    :twitter_account,
-    :ad_client,
-    :ad_slot,
-    :menu_url,
-    :home_url,
-    :footer_url
+  attrs = %i(
+    name
+    js_url
+    css_url
+    fqdn
+    tagline
+    description
+    logo_url
+    favicon_url
+    mobile_favicon_url
+    gtm_id
+    content_header_url
+    promotion_url
+    sns_share_caption
+    twitter_account
+    ad_client
+    ad_slot
+    menu_url
+    home_url
+    footer_url
   )
+
+  permit_params(*attrs)
+
+  form do |f|
+    f.semantic_errors
+    f.inputs(*attrs)
+
+    f.actions
+  end
 
   actions :all, except: :destroy
 end
