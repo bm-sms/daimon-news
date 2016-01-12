3cf6f88570adcf1e035b075a05f0c46188798335
Kenji Okimoto <okimoto@clear-code.com>
2016-01-12 15:52:07 +0900

    Do not show source_url and thumbnail_url

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Post do
   permit_params :site_id, :category_id, :title, :body, :published_at
+
+  form do |f|
+    f.semantic_errors
+    f.inputs except: [:source_url, :thumbnail_url]
+    f.actions
+  end
 end
