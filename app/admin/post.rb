425d3f99d4ec25f68a53927a181ceffbb75440fb
Kenji Okimoto <okimoto@clear-code.com>
2016-01-14 08:47:17 +0900

    Break a long line

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Post do
-  permit_params :site_id, :category_id, :title, :body, :published_at, images_attributes: [:id, :image, :_destroy]
+  permit_params(:site_id, :category_id, :title, :body, :published_at,
+                images_attributes: [:id, :image, :_destroy])
 
   form do |f|
     f.semantic_errors
