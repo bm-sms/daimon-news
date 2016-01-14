1cbbd83d8b479c3cdbd591797681ba623c974bdd
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-01-14 17:01:04 +0900

    Update links seeds

--- db/seeds/development/links.seeds.rb
+++ db/seeds/development/links.seeds.rb
@@ -2,13 +2,18 @@ after 'development:sites' do
   site1 = Site.find_by!(name: 'site1')
 
   site1.links.create!(
-    text:        'exapmle',
-    url:         'http://exapmle.com/',
-    order:       1
+    text:  'about',
+    url:   '/about',
+    order: 1
   )
   site1.links.create!(
-    text:        'sms',
-    url:         'http://bm-sms.co.jp/',
-    order:       2
+    text:  'policy',
+    url:   '/policy',
+    order: 2
+  )
+  site1.links.create!(
+    text:  'company',
+    url:   'http://bm-sms.co.jp/',
+    order: 3
   )
 end
