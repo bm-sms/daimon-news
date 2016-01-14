4783b124c4515c41cdbceee5801a978651575dcd
Kenji Okimoto <okimoto@clear-code.com>
2016-01-14 09:09:09 +0900

    Display image URL after image

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -6,9 +6,13 @@ ActiveAdmin.register Post do
     f.semantic_errors
     f.inputs except: [:source_url, :thumbnail_url]
     f.has_many :images, allow_destroy: true, heading: false, new_record: true do |image_form|
-      image_form.input(:image,
-                       as: :file,
-                       hint: (image_form.template.image_tag(image_form.object.image) if image_form.object.image?))
+      hint_proc = lambda do
+        if image_form.object.image?
+          image_form.template.image_tag(image_form.object.image) +
+            image_form.template.text_field_tag(nil, image_form.object.image_url)
+        end
+      end
+      image_form.input(:image, as: :file, hint: hint_proc.call)
     end
     f.actions
   end
