15b9aabdf06a43ee1ffb8b33e7dd87e51431d4e4
Kenji Okimoto <okimoto@clear-code.com>
2016-01-14 14:56:21 +0900

    Add null constraint to images.post_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114054124) do
+ActiveRecord::Schema.define(version: 20160114055418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20160114054124) do
     t.string   "image",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "post_id"
+    t.integer  "post_id",    null: false
   end
 
   create_table "posts", force: :cascade do |t|
