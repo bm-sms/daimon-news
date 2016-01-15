f884497c5e159d38e2170154e225dbff8fd701a8
okkez <okkez000@gmail.com>
2016-01-15 09:08:48 +0900

    Move favicon related methods to ApplicationHelper
    Because we cannot use decorator methods in layout.

--- app/decorators/site_decorator.rb
+++ app/decorators/site_decorator.rb
@@ -10,22 +10,4 @@ module SiteDecorator
       image: logo_url
     }
   end
-
-  def favicon_tag
-    if favicon_image.present?
-      favicon_link_tag favicon_image_url
-    elsif favicon_url.present?
-      favicon_link_tag favicon_url
-    end
-  end
-
-  def mobile_favicon_tag
-    if mobile_favicon_image.present?
-      favicon_link_tag(mobile_favicon_image_url,
-                       rel: 'apple-touch-icon-precomposed', type: 'image/png')
-    elsif mobile_favicon_url.present?
-      favicon_link_tag(mobile_favicon_url,
-                       rel: 'apple-touch-icon-precomposed', type: 'image/png')
-    end
-  end
 end
