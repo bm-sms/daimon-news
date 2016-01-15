fc602a487c672235e678f57923f260308c41a5fe
Kenji Okimoto <okimoto@clear-code.com>
2016-01-15 17:08:36 +0900

    Break a long line

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Post do
-  permit_params(:site_id, :category_id, :title, :body, :published_at,
+  permit_params(:site_id,
+                :category_id,
+                :title,
+                :body,
+                :published_at,
                 images_attributes: [:id, :image, :_destroy])
 
   form do |f|
