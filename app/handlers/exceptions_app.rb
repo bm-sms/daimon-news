23d7f8ce913075500d26b31c60d2e5c8a44e13cf
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-25 16:48:17 +0900

    Extract CurrentResouceHelper

--- app/handlers/exceptions_app.rb
+++ app/handlers/exceptions_app.rb
@@ -1,7 +1,8 @@
 class ExceptionsApp < Rambulance::ExceptionsApp
-  helper ApplicationHelper
+  include CurrentResouceHelper
 
-  helper_method :current_site, :current_category
+  helper ApplicationHelper
+  helper CurrentResouceHelper
 
   def bad_request
   end
@@ -13,21 +14,5 @@ class ExceptionsApp < Rambulance::ExceptionsApp
   end
 
   def internal_server_error
-  end
-
-  private
-
-  def current_site
-    @current_site ||=
-      if params[:site_id]
-        # for test and debug
-        Site.find(params[:site_id])
-      else
-        Site.find_by!(fqdn: request.server_name)
-      end
-  end
-
-  def current_category
-    nil
   end
 end
---
+++ app/helpers/current_resouce_helper.rb
@@ -0,0 +1,15 @@
+module CurrentResouceHelper
+  def current_site
+    @current_site ||=
+      if params[:site_id]
+        # for test and debug
+        Site.find(params[:site_id])
+      else
+        Site.find_by!(fqdn: request.server_name)
+      end
+  end
+
+  def current_category
+    nil
+  end
+end
