850157a115e87559d1e581ead957585076b12bee
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-01-28 19:06:49 +0900

    Support to update site

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -6,8 +6,40 @@ class Admin::SitesController < Admin::ApplicationController
   def update
     @site = current_site
 
-    # TODO Update
+    if @site.update(site_params)
+      redirect_to [:admin, :root], notice: 'サイト情報が更新されました。'
+    else
+      render :edit
+    end
+  end
 
-    redirect_to [:admin, :root], notice: 'サイト情報が更新されました。'
+  private
+
+  def site_params
+    params.require(:site).permit(
+      :name,
+      :description,
+      :js_url,
+      :css_url,
+      :fqdn,
+      :tagline,
+      :logo_url,
+      :favicon_url,
+      :mobile_favicon_url,
+      :gtm_id,
+      :content_header_url,
+      :promotion_url,
+      :sns_share_caption,
+      :twitter_account,
+      :menu_url,
+      :home_url,
+      :footer_url,
+      :ad_client,
+      :ad_slot,
+      :opened,
+      :logo_image,
+      :favicon_image,
+      :mobile_favicon_image
+    )
   end
 end
