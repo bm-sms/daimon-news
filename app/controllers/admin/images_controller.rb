f1d5a836d4fe2f58e8a88428e681ee87f2eb2b9a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-01-29 16:34:32 +0900

    Prefer whitelist

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ImagesController < Admin::ApplicationController
   def create
     # NOTE bootstrap-markdown-editor がファイルごとにユニークなキーで画像を送信してくる
-    images = params.except(:action, :controller).map do |_, image|
+    images = params.select {|key, _| key =~ /\Afile\d+\z/ }.map do |_, image|
       current_site.images.create!(image: image)
     end
 
