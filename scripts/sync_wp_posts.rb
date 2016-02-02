6109f89bfd5ec750ca39403a76cf6fd9a9c72754
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-02-02 15:45:36 +0900

    Add import script for image in post content

--- scripts/sync_wp_posts.rb
+++ scripts/sync_wp_posts.rb
@@ -43,6 +43,27 @@ class WpPost < WpApplicationRecord
 
     [ASSET_DIR, URI(thumbnail.guid).path.split('/').last(3)].join('/')
   end
+
+  def convert_image_url
+    doc = Nokogiri::HTML(post_content)
+
+    links = {}
+
+    doc.search('img').each do |element|
+      original_src = element['src']
+      new_src = yield(original_src)
+      links[original_src] = new_src
+      element['src'] = new_src
+    end
+
+    links.each do |original_src, new_src|
+      doc.search("a[href='#{original_src}']").each do |element|
+        element['href'] = new_src
+      end
+    end
+
+    self.post_content = doc.search('body')[0].inner_html
+  end
 end
 
 class WpPostmetum < WpApplicationRecord
@@ -92,6 +113,11 @@ target.find_each.with_index do |wp_post, i|
       c.description = wp_post.category_taxonomy.description
       c.slug        = wp_term.slug
       c.order       = site.categories.maximum(:order) + 1
+    end
+
+    wp_post.convert_image_url do |url|
+      image = site.images.create!(remote_image_url: url)
+      image.image_url
     end
 
     post = site.posts.find_or_initialize_by(original_id: wp_post.id)
