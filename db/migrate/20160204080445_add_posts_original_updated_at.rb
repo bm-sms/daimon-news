95a0b6a0c45b26fbfe769a07e0ebec111c5ce2da
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-02-04 18:26:36 +0900

    reversible

--- db/migrate/20160204080445_add_posts_original_updated_at.rb
+++ db/migrate/20160204080445_add_posts_original_updated_at.rb
@@ -2,6 +2,8 @@ class AddPostsOriginalUpdatedAt < ActiveRecord::Migration
   def change
     add_column :posts, :original_updated_at, :datetime
 
-    execute 'UPDATE posts SET original_updated_at = updated_at'
+    reversible do |dir|
+      dir.up { execute 'UPDATE posts SET original_updated_at = updated_at' }
+    end
   end
 end
