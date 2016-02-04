b3fd1a12a5b5cae069969653eacbc61bed940626
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-02-04 16:02:46 +0900

    Add `posts.original_html`

--- db/schema.rb
+++ db/schema.rb
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20160204080445) do
     t.string   "thumbnail"
     t.integer  "author_id"
     t.datetime "original_updated_at"
+    t.text     "original_html"
   end
 
   add_index "posts", ["published_at", "id"], name: "index_posts_on_published_at_and_id", using: :btree
