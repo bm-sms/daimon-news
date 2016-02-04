65c079fe3c2c10e2e13bf4a0771981474eaafb68
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-02-04 18:40:43 +0900

    Remove linefeed from og:description for post

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -8,7 +8,7 @@ module PostDecorator
       locale: 'ja_JP',
       type: 'article',
       title: title,
-      description: render_markdown(body),
+      description: render_markdown(body)&.gsub("\n", ''),
       url: post_url(id, all: true),
       site_name: site.name,
       modified_time: updated_at.to_datetime.to_s,
