9da74da6078fcc6b95da742f30af833dfe4c9a6f
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-02-09 11:03:28 +0900

    Extract models for WP

--- scripts/sync_wp_posts.rb
+++ scripts/sync_wp_posts.rb
@@ -10,92 +10,9 @@ WP_DB_URI = URI(ARGV[0])
 FQDN      = ARGV[1]
 ASSET_DIR = ARGV[2]
 
-class WpApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-end
+load Rails.root.join('lib/wp_models.rb')
 
-WpApplicationRecord.establish_connection(
-  adapter:  'mysql2',
-  host:     WP_DB_URI.host,
-  username: WP_DB_URI.user,
-  password: WP_DB_URI.password,
-  database: WP_DB_URI.path[1..-1]
-)
-
-class WpPost < WpApplicationRecord
-  has_many :wp_term_relationships, foreign_key: :object_id
-  has_many :wp_term_taxonomies, through: :wp_term_relationships
-
-  has_many :wp_postmeta, foreign_key: :post_id
-
-  attr_accessor :post_content_with_updated_image_url
-
-  def category_taxonomy
-    @category_taxonomy ||= wp_term_taxonomies.find_by(taxonomy: 'category')
-  end
-
-  def thumbnail
-    return @thumbnail if defined? @thumbnail
-
-    @thumbnail = WpPost.find_by(id: wp_postmeta.find_by(meta_key: '_thumbnail_id').try!(:meta_value))
-  end
-
-  def thumbnail_url
-    return unless thumbnail
-
-    [ASSET_DIR, URI(thumbnail.guid).path.split('/').last(3)].join('/')
-  end
-
-  def convert_image_url
-    doc = Nokogiri::HTML(post_content)
-
-    links = {}
-
-    doc.search('img').each do |element|
-      original_src = element['src']
-      new_src = yield(original_src)
-      links[original_src] = new_src
-      element['src'] = new_src
-    end
-
-    links.each do |original_src, new_src|
-      doc.search("a[href='#{original_src}']").each do |element|
-        element['href'] = new_src
-      end
-    end
-
-    self.post_content_with_updated_image_url = doc.search('body')[0].inner_html
-  end
-
-  def markdown_body
-    ReverseMarkdown.convert(post_content_with_updated_image_url).gsub("\r", "\n")
-  end
-end
-
-class WpPostmetum < WpApplicationRecord
-end
-
-class WpTermRelationship < WpApplicationRecord
-  belongs_to :wp_post, foreign_key: :object_id
-  belongs_to :wp_term_taxonomy, foreign_key: :term_taxonomy_id
-end
-
-class WpTermTaxonomy < WpApplicationRecord
-  self.table_name = :wp_term_taxonomy
-  self.primary_key = :term_taxonomy_id
-
-  has_many :wp_term_relationships, foreign_key: :term_taxonomy_id
-  has_many :wp_posts, through: :wp_term_relationships
-
-  belongs_to :wp_term, foreign_key: :term_id
-end
-
-class WpTerm < WpApplicationRecord
-  self.primary_key = :term_id
-
-  has_many :wp_term_taxonomies, foreign_key: :term_id
-  has_many :wp_posts, through: :wp_term_taxonomies
-end
+WpApplicationRecord.connect_to(WP_DB_URI)
 
 site = Site.find_by!(fqdn: FQDN)
 
@@ -133,7 +50,7 @@ target.find_each.with_index do |wp_post, i|
       published_at:         wp_post.post_date_gmt,
       body:                 wp_post.markdown_body,
       category:             category,
-      remote_thumbnail_url: wp_post.thumbnail_url,
+      remote_thumbnail_url: wp_post.thumbnail_url(ASSET_DIR),
       original_updated_at:  wp_post.post_modified_gmt,
       original_html:        wp_post.post_content,
       updated_at:           wp_post.post_modified_gmt
