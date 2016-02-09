66848980e9ce62a1b6027908212825f57b4ba6ad
Kenji Okimoto <okimoto@clear-code.com>
2016-02-09 14:23:28 +0900

    Add factories

---
+++ test/factories/category.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :category do
+    name "category1"
+    description "category1 description\n"
+    slug "category1"
+    order 1
+  end
+end
---
+++ test/factories/site.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :site do
+    name "daimon-news"
+    fqdn "www.example.com"
+  end
+end
