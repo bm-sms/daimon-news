de6959d0eb9812030f3890361fa313c094bc98b3
Kenji Okimoto <okimoto@clear-code.com>
2016-02-09 14:23:59 +0900

    Add test for category route

--- test/routes/category_route_test.rb
+++ test/routes/category_route_test.rb
@@ -1,4 +1,18 @@
 class CategoryRouteTest < ActionDispatch::IntegrationTest
+  def setup
+    @site = create(:site)
+    @category = create(:category, site_id: @site.id)
+  end
+
+  def teardown
+    @site.destroy
+  end
+
+  def test_first_page
+    get "/category/category1"
+    assert_response(:success)
+  end
+
   def test_redirect_to_2nd_page
     get "/category/category1/page/2"
     assert_response(:moved_permanently)
