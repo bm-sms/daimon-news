6c21ad8f68e3a9e7cae59cafe9a871c76ee874b7
Kenji Okimoto <okimoto@clear-code.com>
2016-02-09 15:53:25 +0900

    Add page number to category#show breadcrumb

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,6 +6,11 @@ crumb :category do |category|
   link category.name, category_url(category.slug)
 end
 
+crumb :category_page do |category|
+  link params[:page]
+  parent :category, category
+end
+
 crumb :post do |post|
   link post.title, post_url(post)
   parent :category, post.category
