2e295e3a83238ea3472233a439561fb3ab5bb1de
Kenji Okimoto <okimoto@clear-code.com>
2016-02-12 16:18:34 +0900

    Add line breaks to close to the original behavior

--- lib/daimon/render/html.rb
+++ lib/daimon/render/html.rb
@@ -3,9 +3,9 @@ module Daimon
     class HTML < Redcarpet::Render::HTML
       def block_quote(quote)
         blockquotes = quote.each_line("").map do |paragraph|
-            "<blockquote>\n#{paragraph.strip}\n</blockquote>"
+            "\n<blockquote>\n#{paragraph.strip}\n</blockquote>"
         end
-        blockquotes.join("\n")
+        "#{blockquotes.join("\n")}\n"
       end
     end
   end
