12a2862c06b919e8faf2fc2b68df5201e758fb62
Kenji Okimoto <okimoto@clear-code.com>
2016-02-12 18:17:24 +0900

    Stop customizing `block_quote`
    In previous version, many records are broken.
    In this version(w/o customize), broken records are not so many.

--- lib/daimon/render/html.rb
+++ lib/daimon/render/html.rb
@@ -1,12 +1,6 @@
 module Daimon
   module Render
     class HTML < Redcarpet::Render::HTML
-      def block_quote(quote)
-        blockquotes = quote.each_line("").map do |paragraph|
-            "\n<blockquote>\n#{paragraph.strip}\n</blockquote>"
-        end
-        "#{blockquotes.join("\n")}\n"
-      end
     end
   end
 end
