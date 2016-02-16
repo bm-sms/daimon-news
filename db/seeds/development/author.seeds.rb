b47916ff7a553db0b07ad4bc1fb91bd5b7806db7
Kenji Okimoto <okimoto@clear-code.com>
2016-02-16 15:59:07 +0900

    Add seed for authors

---
+++ db/seeds/development/author.seeds.rb
@@ -0,0 +1,24 @@
+after 'development:sites' do
+  site1 = Site.find_by!(name: 'site1')
+
+  site1.authors.create!(
+    name: 'name11',
+    description: "description11"
+  )
+  site1.authors.create!(
+    name: 'name12',
+    description: "description12"
+  )
+
+
+  site2 = Site.find_by!(name: 'site2')
+
+  site2.authors.create!(
+    name: 'name21',
+    description: "description21"
+  )
+  site1.authors.create!(
+    name: 'name22',
+    description: "description22"
+  )
+end
