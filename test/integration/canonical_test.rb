facbfac82772d3674bf252af63606b41eef99d64
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-02-17 11:04:18 +0900

    Add canonical for category page

--- test/integration/canonical_test.rb
+++ test/integration/canonical_test.rb
@@ -22,4 +22,22 @@ class CanonicalTest < ActionDispatch::IntegrationTest
 
     assert_equal "http://#{@post.site.fqdn}/#{@post.id}?all=true", find('link[rel=canonical]', visible: false)[:href]
   end
+
+  sub_test_case 'category page' do
+    setup do
+      @category = create(:category, site: @post.site)
+    end
+
+    test 'Cannonical must be shown' do
+      visit "/category/#{@category.slug}"
+      assert_equal "http://#{@post.site.fqdn}/category/#{@category.slug}",
+                   find('link[rel=canonical]', visible: false)[:href]
+    end
+
+    test 'Cannonical must be unified for pagenation' do
+      visit "/category/#{@category.slug}?page=2"
+      assert_equal "http://#{@post.site.fqdn}/category/#{@category.slug}",
+                   find('link[rel=canonical]', visible: false)[:href]
+    end
+  end
 end
