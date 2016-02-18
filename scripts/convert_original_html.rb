8e1344afbcac33c79376bbb46a603db4d9c07b80
Kenji Okimoto <okimoto@clear-code.com>
2016-02-18 11:06:46 +0900

    Add scripts/convert_original_html.rb

---
+++ scripts/convert_original_html.rb
@@ -0,0 +1,50 @@
+require "wp_html_util"
+
+parser = OptionParser.new
+
+parser.banner = <<BANNER
+Usage
+  $ bin/rails r scripts/convert_original_html.rb [runner options] -- [options] <fqdn>
+BANNER
+
+argv = ARGV.dup
+argv.delete("--")
+
+begin
+  parser.parse!(argv)
+rescue OptionParser::ParseError => ex
+  $stderr.puts ex.message
+  $stderr.puts parser.help
+  exit(false)
+end
+
+Post.class_eval do
+  include WpHTMLUtil
+
+  def preprocess(&block)
+    super(stripped_html, &block)
+  end
+end
+
+
+fqdn = argv[0]
+
+site = Site.find_by!(fqdn: fqdn)
+
+site.transaction do
+  site.posts.each do |post|
+    puts post.public_id
+    doc = Nokogiri::HTML(post.original_html)
+    article = doc.search("article")
+    article.search("aside").remove
+    article.search("#breadcrumb").remove
+    article.search(".ad.banner").remove
+    article.search("span.kdate").remove
+    post.stripped_html = article.inner_html.gsub(/<!--.*-->/, "")
+    post.replaced_html = post.preprocess do |url|
+      image = site.images.create!(remote_image_url: url)
+      image.image_url
+    end
+    post.save!
+  end
+end
