5bcee6c4daa9dd58d96aab6b0de9ea9d93d6495f
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-02-22 14:24:30 +0900

    Fix rss
    - show truncated contents
    - display thumbnail

--- app/views/feed/show.xml.builder
+++ app/views/feed/show.xml.builder
@@ -8,7 +8,7 @@ xml.rss('version' => '2.0', 'xmlns:dc' => 'http://purl.org/dc/elements/1.1/') do
     @posts.each do |post|
       xml.item do
         xml.title post.title
-        xml.description render_markdown(post.body)
+        xml.description render(partial: 'post.html', object: post)
         xml.category post.category.name
         xml.pubDate post.published_at.to_s(:rfc822)
         xml.guid post_url(post), isPermaLink: true
---
+++ test/integration/rss_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class RssTest < ActionDispatch::IntegrationTest
+  setup do
+    @post = create(:post, body: <<~EOS)
+      # Lorem ipsum dolor sit amet,
+
+      consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+      - Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      - Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    EOS
+
+    switch_domain(@post.site.fqdn)
+  end
+
+  test '' do
+    visit '/feed.xml'
+
+    within 'item' do
+      assert_equal @post.title, find('title').text
+      assert_equal %|<img src="#{@post.thumbnail_url}" alt="Thumbnail" />|, find('description').text.scan(%r|<img.*/>|).first
+      description = find('description').text.scan(%r|<p>(.*)</p>|).first.first
+      assert_equal "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad mi...",
+        description
+      assert_equal 140, description.length
+    end
+  end
+end
