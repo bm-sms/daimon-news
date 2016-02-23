eaf237fc552d5d77a4a9f0a6a92cba34f82ecadf
Kenji Okimoto <okimoto@clear-code.com>
2016-02-23 11:30:42 +0900

    Avoid thumbnail null constraint

--- scripts/convert_image.rb
+++ scripts/convert_image.rb
@@ -1,4 +1,5 @@
 require "optparse"
+require "wp_html_util"
 
 parser = OptionParser.new
 
@@ -27,23 +28,42 @@ end
 
 fqdn = argv[0]
 
-Post.class_eval do
+class LitePost < ActiveRecord::Base
   include WpHTMLUtil
+
+  self.table_name = :posts
+
+  belongs_to :site
+  belongs_to :category
+  belongs_to :author
+
+  validates :body, presence: true
+  validates :thumbnail, presence: true
+
+  before_save :assign_public_id
+
+  def assign_public_id
+    self.public_id ||= (self.class.maximum(:public_id) || 0) + 1
+  end
 
   def preprocess(&block)
     super(original_source, &block)
   end
 end
 
+Site.class_eval do
+  has_many :lite_posts
+end
+
 site = Site.find_by!(fqdn: fqdn)
 
 posts = if force
-  site.posts.all
+  site.lite_posts
 else
-  site.posts.where(replaced_html: nil).all
+  site.lite_posts.where(replaced_html: nil)
 end
 
-site.posts.all.each do |post|
+posts.all.each do |post|
   puts post.public_id
   site.transaction do
     replaced_html = post.preprocess do |url|
