9c1def106d328ea2522958bddcb346c9d8715598
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-02-25 16:52:21 +0900

    Show credits to posts#show

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -1,7 +1,7 @@
 class Credit < ActiveRecord::Base
   belongs_to :post
   belongs_to :participant
-  belongs_to :credit_role
+  belongs_to :role, foreign_key: :credit_role_id, class_name: 'CreditRole'
 
   validates :post_id, presence: true
   validates :participant_id, presence: true
