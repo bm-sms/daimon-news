ba4cd09753687eda2116fb3891e3b678a1180618
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-02 08:25:27 +0900

    Move search code to SearchController from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,5 @@ class ApplicationController < ActionController::Base
   def prepare_search
     @query = Query.new(params[:query])
     @query.site_id = current_site.id
-    searcher = PostSearcher.new
-    @result_set = searcher.search(@query, page: params[:page])
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
   def search
+    searcher = PostSearcher.new
+    @result_set = searcher.search(@query, page: params[:page])
   end
 end
