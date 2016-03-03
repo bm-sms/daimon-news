a0a81cff9076b72b90cf3e71c49756fae5971173
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-03 18:49:01 +0900

    Use strong parameters for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
   def search
-    @query = Query.new(params[:query])
+    @query = Query.new(search_params[:query])
     @query.site_id = current_site.id
     searcher = PostSearcher.new
-    @result_set = searcher.search_with_fallback_to_first_page(@query, page: params[:page])
+    @result_set = searcher.search_with_fallback_to_first_page(@query, page: search_params[:page])
+  end
+
+  def search_params
+    params.permit(:page, query: [:keywords, :participant_id, :site_id])
   end
 end
