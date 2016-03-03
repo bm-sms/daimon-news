f0bfb2392e0c2b50ed63add3800ac79812ea30d8
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-03 18:08:47 +0900

    Use form_tag instead of form_for not to use query object for search form in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
     before_action :authorize_user!, if: :current_user
   end
 
-  before_action :prepare_search
-
   include CurrentResouceHelper
 
   private
@@ -25,10 +23,5 @@ class ApplicationController < ActionController::Base
 
   def authorize_user!
     head :forbidden unless current_user.admin? || current_user.editor_of?(current_site)
-  end
-
-  def prepare_search
-    @query = Query.new(params[:query])
-    @query.site_id = current_site.id
   end
 end
