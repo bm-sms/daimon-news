b6789490cfa3fca41f155f5cf1eca121cc7d802a
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-04 08:20:56 +0900

    Increase credits seeds

--- db/seeds/development/credits.seeds.rb
+++ db/seeds/development/credits.seeds.rb
@@ -1,11 +1,11 @@
 after 'development:credit_roles', 'development:participants', 'development:posts' do
   site1 = Site.find_by!(name: 'site1')
-  participant = site1.participants.first!
+  participants = site1.participants.cycle
   credit_role = site1.credit_roles.first!
 
-  site1.posts.published.order_by_recently.limit(3).each do |post|
+  site1.posts.published.order_by_recently.limit(10).each do |post|
     post.credits.create!(
-      participant: participant,
+      participant: participants.next,
       role: credit_role
     )
   end
