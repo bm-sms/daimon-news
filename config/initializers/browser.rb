461dc69abaf1868e7e55212972b666ca7ddde4a8
Kenji Okimoto <okimoto@clear-code.com>
2016-03-04 14:57:35 +0900

    Stop redirection from /admin/*, /editor/* to /admin, /editor

--- config/initializers/browser.rb
+++
@@ -1,9 +0,0 @@
-Rails.configuration.middleware.use Browser::Middleware do
-  next unless browser.ie?
-  case request.env["PATH_INFO"]
-  when %r!/admin/.+!
-    redirect_to admin_root_path
-  when %r!/editor/.+!
-    redirect_to editor_root_path
-  end
-end
