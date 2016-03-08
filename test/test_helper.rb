989d9498bd65fbe9b4a405d8dc3c6451ec07f823
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-08 12:05:18 +0900

    test: add Groonga database setup/teardown methods

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,24 @@ Capybara.javascript_driver = :poltergeist
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include FactoryGirl::Syntax::Methods
+
+  def setup_groonga_database
+    ENV['GROONGA_DATABASE_PATH'] ||= groonga_database_path
+    FileUtils.rm_rf(groonga_database_dir)
+    FileUtils.mkdir_p(groonga_database_dir)
+    Groonga::Database.create(path: groonga_database_path)
+    load Rails.root.join("groonga/init.rb")
+  end
+
+  def teardown_groonga_database
+    FileUtils.rm_rf(groonga_database_dir)
+  end
+
+  def groonga_database_path
+    File.join(groonga_database_dir, "#{Rails.env}.db")
+  end
+
+  def groonga_database_dir
+    Rails.root.join("groonga/data/#{Rails.env}").to_s
+  end
 end
