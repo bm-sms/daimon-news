21b1dd3cd412e8f5ce8f1a2759c82de978fc1d10
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-03-10 19:33:13 +0900

    First impl (by @kou)

--- app/groonga/post_search_result_set.rb
+++ app/groonga/post_search_result_set.rb
@@ -2,4 +2,24 @@ class PostSearchResultSet
   attr_accessor :posts
   attr_accessor :participants
   attr_accessor :categories
+
+  def initialize
+    @snippet = nil
+  end
+
+  def snippet(text)
+    (@snippet ||= create_snippet).execute(text)
+  end
+
+  private
+  def create_snippet
+    open_tag = '<span class="keyword">'
+    close_tag = '</span>'
+    options = {
+      :normalize => true,
+      :width => 200,
+      :html_escape => true,
+    }
+    @posts.expression.snippet([open_tag, close_tag], options)
+  end
 end
