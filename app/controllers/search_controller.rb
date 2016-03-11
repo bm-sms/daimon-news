605e0dc3b0ae405791331b6e7dace31d36339123
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-11 13:01:14 +0900

    Break a long line

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,11 @@ class SearchController < ApplicationController
     searcher = PostSearcher.new
     @result_set = searcher.search(@query)
     searched_post_ids = @result_set.posts.map(&:_key)
-    @posts = Post.includes(:category, credits: [:participant]).published.where(id: searched_post_ids).order_by_recently.page(params[:page]).per(50)
+    @posts = Post.includes(:category, credits: [:participant])
+               .published.where(id: searched_post_ids)
+               .order_by_recently
+               .page(params[:page])
+               .per(50)
   end
 
   private
