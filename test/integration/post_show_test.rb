c58314241b2b3fb47c5377821c91e078ae59f7e2
Kenji Okimoto <okimoto@clear-code.com>
2016-03-18 09:28:11 +0900

    Add test for displaying credits in page 2

--- test/integration/post_show_test.rb
+++ test/integration/post_show_test.rb
@@ -3,8 +3,18 @@ require 'test_helper'
 class PostShowTest < ActionDispatch::IntegrationTest
   setup do
     @post = create(:post, :whatever, :with_credit, body: <<~EOS.encode(crlf_newline: true))
-      <h1> hi </h1>
+      # hi
+
       contents
+
+      <!--nextpage-->
+
+      ## hi hi
+
+      contents
+      contents
+      contents
+
     EOS
 
     switch_domain(@post.site.fqdn)
@@ -25,6 +35,15 @@ class PostShowTest < ActionDispatch::IntegrationTest
     within '.post__body' do
       assert page.has_selector?('h1', text: 'hi')
       assert page.has_selector?('section.credits')
+    end
+  end
+
+  test "page 2 should not display credits" do
+    visit "/#{@post.public_id}?page=2"
+
+    within '.post__body' do
+      assert page.has_selector?('h2', text: 'hi hi')
+      assert page.has_no_selector?('section.credits')
     end
   end
 end
