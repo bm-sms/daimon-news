270eaac8291aef1cdb1ea2ceb58028d2a27a2ec0
Kenji Okimoto <okimoto@clear-code.com>
2016-03-22 11:13:15 +0900

    Use Kaminarized object for `@post.pages`

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 
     @pages =
       if params[:all]
-        @post.pages
+        Kaminari.paginate_array(@post.pages).page(1).per(@post.pages.size)
       else
         Kaminari.paginate_array(@post.pages).page(params[:page]).per(1)
       end
