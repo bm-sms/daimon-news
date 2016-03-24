28235580572541373a9163aa59b1159576e8820d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-03-24 22:13:20 +0900

    Make breadcrumbs iikanzi

--- test/integration/breadcrumbs_test.rb
+++ test/integration/breadcrumbs_test.rb
@@ -16,9 +16,11 @@ class BreadcrumbsTest < ActionDispatch::IntegrationTest
 
     click_on '次へ'
 
-    assert_equal 'Press 2ページ目', find('.breadcrumbs').text
-    assert_equal 2, all('.breadcrumbs__fragment').count
-    assert_equal "http://#{@site.fqdn}/", find('.breadcrumbs__link')[:href]
+    # TODO xxx/yyy の運用をやめたらテストからも xxx を取り除く
+
+    assert_equal 'Press 2ページ目', find('.xxx .breadcrumbs').text
+    assert_equal 2, all('.xxx .breadcrumbs__fragment').count
+    assert_equal "http://#{@site.fqdn}/", find('.xxx .breadcrumbs__link')[:href]
   end
 
   test 'Category page has page number breadcrumbs except first page' do
@@ -26,13 +28,15 @@ class BreadcrumbsTest < ActionDispatch::IntegrationTest
       click_on @category.name
     end
 
-    assert_equal "Press #{@category.name}", find('.breadcrumbs').text
+    # TODO xxx/yyy の運用をやめたらテストからも xxx を取り除く
+
+    assert_equal "Press #{@category.name}", find('.xxx .breadcrumbs').text
 
     click_on '次へ'
 
-    assert_equal "Press #{@category.name} 2ページ目", find('.breadcrumbs').text
-    assert_equal 3, all('.breadcrumbs__fragment').count
-    assert_equal "http://#{@site.fqdn}/", find('.breadcrumbs__fragment:nth-child(1) .breadcrumbs__link')[:href]
-    assert_equal "http://#{@site.fqdn}/category/#{@category.slug}", find('.breadcrumbs__fragment:nth-child(2) .breadcrumbs__link')[:href]
+    assert_equal "Press #{@category.name} 2ページ目", find('.xxx .breadcrumbs').text
+    assert_equal 3, all('.xxx .breadcrumbs__fragment').count
+    assert_equal "http://#{@site.fqdn}/", find('.xxx .breadcrumbs__fragment:nth-child(1) .breadcrumbs__link')[:href]
+    assert_equal "http://#{@site.fqdn}/category/#{@category.slug}", find('.xxx .breadcrumbs__fragment:nth-child(2) .breadcrumbs__link')[:href]
   end
 end
