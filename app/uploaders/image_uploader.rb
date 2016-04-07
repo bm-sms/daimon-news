b5e42bb280d4085deb5ac38a9176ec9287fbfa0e
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-04-07 10:57:30 +0900

    Use shared S3 bucket on Review Apps

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,6 +6,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    [store_root_dir, "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"].compact.join("/")
+  end
+
+  private
+
+  def store_root_dir
+    ENV["HEROKU_APP_NAME"] if ENV["USE_SHARED_S3_BUCKET"]
   end
 end
