1a76dab6652fa847e7f57930b7d38a480d28eafb
Kenji Okimoto <okimoto@clear-code.com>
2016-04-12 17:03:41 +0900

    Add controller and views for serials

---
+++ app/controllers/editor/serials_controller.rb
@@ -0,0 +1,59 @@
+class Editor::SerialsController < Editor::ApplicationController
+  def index
+    @serials = serials.order(id: :desc)
+  end
+
+  def show
+    @serial = serials.find(params[:id])
+  end
+
+  def new
+    @serial = serials.build
+  end
+
+  def create
+    @serial = serials.build(serial_params)
+
+    if @serial.save
+      redirect_to [:editor, @serial], notice: "連載が作成されました"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @serial = serials.find(params[:id])
+  end
+
+  def update
+    @serial = serials.find(params[:id])
+
+    if @serial.update(serial_params)
+      redirect_to [:editor, @serial], notice: "連載が更新されました"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @serial = serials.find(params[:id])
+
+    @serial.destroy
+
+    redirect_to editor_serials_url, notice: "連載が削除されました"
+  end
+
+  private
+
+  def serials
+    current_site.serials
+  end
+
+  def serial_params
+    params.require(:serial).permit(
+      :title,
+      :description,
+      :slug
+    )
+  end
+end
