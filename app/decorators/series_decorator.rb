71fb23b8ef004a82bbace194a9c33dadb8e72e76
Kenji Okimoto <okimoto@clear-code.com>
2016-04-12 14:35:52 +0900

    Add view and controllers related to `/series`
    This is based on category.

---
+++ app/decorators/series_decorator.rb
@@ -0,0 +1,33 @@
+module SeriesDecorator
+  def to_og_params
+    {
+      locale: "ja_JP",
+      type: "object",
+      title: [title, site.tagline, site.name].select(&:present?).join(" | "),
+      description: MetaTags::TextNormalizer.normalize_description(description),
+      url: series_url(slug),
+      site_name: site.name,
+      image: site.logo_image_url
+    }
+  end
+
+  def canonical_url(posts)
+    if posts.current_page > 1
+      series_url(page: posts.current_page)
+    else
+      series_url
+    end
+  end
+
+  def page_title(posts)
+    if posts.current_page > 1
+      "#{title} (#{posts.page_entries_info})"
+    else
+      title
+    end
+  end
+
+  def title_with_n_posts(posts)
+    "#{title} (#{posts.size})"
+  end
+end
--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -6,6 +6,15 @@ crumb :category do |category|
   link category.name, category_url(category.slug)
 end
 
+crumb :series_index do |series_list|
+  link "連載一覧", series_index_url
+end
+
+crumb :series do |series|
+  link series.title, series_url(series.slug)
+  parent :series_index
+end
+
 crumb :post do |post|
   link post.title, post_url(post)
   parent :category, post.category
