69764a89659a55f5b47ad9cce575c5b7e8e2f9ff
Ryunosuke SATO <tricknotes.rs@gmail.com>
2016-04-14 19:49:28 +0900

    Assert CSS content

--- test/controllers/custom_css_controller_test.rb
+++ test/controllers/custom_css_controller_test.rb
@@ -13,20 +13,21 @@ class CustomCssControllerTest < ActionController::TestCase
     site = create(:site, base_hue: 120)
     get :show, fqdn: site.fqdn, digest: "some-values"
 
-    assert_response :success
+    assert_response(:success)
+    assert_match(".category{", @response.body)
   end
 
   test "show should redirect to default css URL without Site#base_hue" do
     site = create(:site, base_hue: nil)
     get :show, fqdn: site.fqdn, digest: "some-values"
 
-    assert_redirected_to %r{/assets/themes/default/application-[a-f\d]+\.js$}
+    assert_redirected_to(%r{/assets/themes/default/application-[a-f\d]+\.js$})
   end
 
   test "show should redirect custom css URL with Site#css_url" do
     site = create(:site, css_url: "http://example.com/test.css", base_hue: 200)
     get :show, fqdn: site.fqdn, digest: "some-values"
 
-    assert_redirected_to "http://example.com/test.css"
+    assert_redirected_to("http://example.com/test.css")
   end
 end
