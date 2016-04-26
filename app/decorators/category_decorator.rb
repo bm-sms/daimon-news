d07c5a09d090a7f6f4beba80ac829b66e99fa164
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-04-26 17:58:04 +0900

    Render as Markdown for category description

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -4,7 +4,7 @@ module CategoryDecorator
       locale: "ja_JP",
       type: "object",
       title: [name, site.tagline, site.name].select(&:present?).join(" | "),
-      description: MetaTags::TextNormalizer.normalize_description(description),
+      description: MetaTags::TextNormalizer.normalize_description(render_markdown(description)),
       url: category_url(slug),
       site_name: site.name,
       image: site.logo_image_url
