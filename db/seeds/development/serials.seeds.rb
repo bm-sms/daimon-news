55c0a5103e3c1895f764ccfca0e975f37e0fbd5c
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-05-12 11:17:18 +0900

    Add seeds for thumbnail of serials

--- db/seeds/development/serials.seeds.rb
+++ db/seeds/development/serials.seeds.rb
@@ -4,7 +4,11 @@ after "development:posts" do
   posts = site1.posts.published.order_by_recently.cycle
 
   5.times do |i|
-    serial = site1.serials.create!(title: "Serial#{i}", description: "Serial")
+    serial = site1.serials.create!(
+      title: "Serial#{i}",
+      description: "Serial",
+      thumbnail: open(Rails.root.join("db/data/thumbnail.jpg"))
+    )
     3.times do
       posts.next.update!(serial: serial)
     end
