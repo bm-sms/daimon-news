66da77fc87c1b4e5329bef50feb6e17eff2fc24b
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-05-13 10:50:06 +0900

    Add assertions for title for serials

--- test/integration/serial_test.rb
+++ test/integration/serial_test.rb
@@ -2,19 +2,23 @@ require "test_helper"
 
 class SerialTest < ActionDispatch::IntegrationTest
   setup do
-    site = create(:site)
-    create_list(:serial, 3, :with_posts, site: site)
+    @site = create(:site)
+    create_list(:serial, 3, :with_posts, site: @site)
 
-    switch_domain(site.fqdn)
+    switch_domain(@site.fqdn)
   end
 
   test "/serials" do
     visit "/serials"
 
+    assert_equal("すべての連載 | #{@site.name}", title)
+
     serial_titles = find_all ".serial-summary__title"
     assert_equal(["Serial 3 (1)", "Serial 2 (1)", "Serial 1 (1)"], serial_titles.map(&:text))
 
     click_on "Serial 3"
+
+    assert_equal("Serial 3 | #{@site.name}", title)
 
     header = find ".serial-header"
     assert_equal("Serial 3", header.text)
