8aa56cf7e6c40f00e7155a1dc0464f6948637422
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-05-24 16:29:47 +0900

    test: Separate the number of posts from the title
    ref: 10361dcad267b0ec8e077549b295dd5bc2543330

--- test/integration/serial_test.rb
+++ test/integration/serial_test.rb
@@ -21,7 +21,14 @@ class SerialTest < ActionDispatch::IntegrationTest
       visit "/serials"
 
       serial_titles = find_all ".serial-summary__title"
-      assert_equal(["Serial 3 (1)", "Serial 2 (1)", "Serial 1 (1)"], serial_titles.map(&:text))
+      assert_equal(["Serial 3", "Serial 2", "Serial 1"], serial_titles.map(&:text))
+    end
+
+    test "number of posts" do
+      visit "/serials"
+
+      number_of_posts = find_all ".number-of-posts"
+      assert_equal(["記事数：1", "記事数：1", "記事数：1"], number_of_posts.map(&:text))
     end
   end
 
