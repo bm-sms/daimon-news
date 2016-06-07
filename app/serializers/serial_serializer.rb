06c4841f300d29be8b706c9b8393d0bd3c5effc4
Kenji Okimoto <okimoto@clear-code.com>
2016-06-07 18:55:53 +0900

    Display serial thumbnail properly

--- app/serializers/serial_serializer.rb
+++ app/serializers/serial_serializer.rb
@@ -2,4 +2,8 @@ class SerialSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :thumbnail_url
 
   has_many :posts
+
+  def thumbnail_url
+    scope.image_url(object.thumbnail_url)
+  end
 end
