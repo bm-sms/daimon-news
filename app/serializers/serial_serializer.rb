403ae3ec9ac032114b1e39263cd5bf7b58da7e46
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-06-23 19:07:50 +0900

    Add posts_count as meta for serials page

--- app/serializers/serial_serializer.rb
+++ app/serializers/serial_serializer.rb
@@ -1,5 +1,10 @@
 class SerialSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :thumbnail_url
+  meta do
+    {
+      posts_count: object.posts.published.count
+    }
+  end
 
   def thumbnail_url
     scope.image_url(object.thumbnail_url)
