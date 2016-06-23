2a498073e095ae82cedd996e61ada7db9fce6a7a
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-06-23 17:18:00 +0900

    Remove unnecessary relationship

--- app/controllers/api/serials_controller.rb
+++ app/controllers/api/serials_controller.rb
@@ -2,7 +2,7 @@ class Api::SerialsController < Api::ApplicationController
   def index
     serials = current_site.serials.includes(:posts).order(id: :desc).page(params.dig(:page, :number)).per(params.dig(:page, :size))
 
-    render json: serials, include: "post"
+    render json: serials
   end
 
   def show
--- app/serializers/serial_serializer.rb
+++ app/serializers/serial_serializer.rb
@@ -1,8 +1,6 @@
 class SerialSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :thumbnail_url
 
-  has_many :posts
-
   def thumbnail_url
     scope.image_url(object.thumbnail_url)
   end
