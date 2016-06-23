aefd13a3c686abaa3aaadfb4f1ff58d7d18e1619
Kenji Okimoto <okimoto@clear-code.com>
2016-06-23 18:27:31 +0900

    Use `extract_plain_text` to render plain text in `/serials`

--- test/integration/serial_test.rb
+++ test/integration/serial_test.rb
@@ -37,6 +37,18 @@ class SerialTest < ActionDispatch::IntegrationTest
       number_of_posts = find_all ".number-of-posts"
       assert_equal(["記事数：1", "記事数：1", "記事数：1"], number_of_posts.map(&:text))
     end
+
+    test "markdown" do
+      create(:serial, :with_posts, site: @site, description: <<~DESC)
+      # h1
+
+      contents
+      DESC
+      visit "/serials"
+
+      description = find_all(".serial-summary__content").first
+      assert_equal("h1 contents", description.text)
+    end
   end
 
   sub_test_case "/serials/:id" do
@@ -55,6 +67,24 @@ class SerialTest < ActionDispatch::IntegrationTest
 
       title = find ".serial-content__title"
       assert_equal(@serials.last.title, title.text)
+    end
+
+    test "serial description" do
+      serial = create(:serial, :with_posts, title: "serial title", site: @site, description: <<~DESC)
+      # h1
+
+      contents
+      DESC
+      visit "/serials"
+
+      click_on serial.title
+
+      within ".serial-content__description" do
+        h1 = find "h1"
+        assert_equal("h1", h1.text)
+        contents = find "p"
+        assert_equal("contents", contents.text)
+      end
     end
   end
 end
