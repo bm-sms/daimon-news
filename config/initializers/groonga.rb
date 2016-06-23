d998ea5d28f50f429eb2c59d7c0e0ac0e726aa0a
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-06-23 15:24:50 +0900

    Use Groonga::Logger.path instead of Groonga::Logger.register to prevent crash

--- config/initializers/groonga.rb
+++ config/initializers/groonga.rb
@@ -1,28 +1,7 @@
 require "fileutils"
 require "groonga"
 
-log_options = {
-  location: false,
-}
-Groonga::Logger.register(log_options) do |event, level, _, _, message, _|
-  if event == :log
-    formatted_message = "Groonga|#{message}"
-    case level
-    when :emergency, :alert, :critical
-      Rails.logger.fatal(formatted_message)
-    when :error
-      Rails.logger.fatal(formatted_message)
-    when :warning
-      Rails.logger.warn(formatted_message)
-    when :notice, :info
-      Rails.logger.info(formatted_message)
-    when :debug, :dump
-      Rails.logger.debug(formatted_message)
-    else
-      Rails.logger.unknown(formatted_message)
-    end
-  end
-end
+Groonga::Logger.path = "tmp/groonga.log"
 
 database_path = ENV["GROONGA_DATABASE_PATH"] || "groonga/data/db"
 if File.exist?(database_path)
