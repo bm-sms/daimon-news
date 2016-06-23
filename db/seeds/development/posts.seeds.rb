761f3bd4629861f6b138b13b281921c46aeaea3b
Kenji Okimoto <okimoto@clear-code.com>
2016-06-23 16:29:06 +0900

    Add seed data `post.id != post.public_id`
    In previous version, all posts are `id == public_id`.

--- db/seeds/development/posts.seeds.rb
+++ db/seeds/development/posts.seeds.rb
@@ -28,7 +28,7 @@ after "development:sites", "development:categories" do
     category: site1.categories.find_by!(slug: "category2"),
   )
 
-  100.times do |i|
+  50.times do |i|
     i += 100
 
     site1.posts.create!(
@@ -47,6 +47,43 @@ after "development:sites", "development:categories" do
       thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
       published_at: Time.current,
       category: site1.categories.find_by!(slug: "category1"),
+    )
+  end
+
+  # below records: id != public_id
+  site1.posts.create!(
+    title: "Hello",
+    body: <<-EOS.strip_heredoc,
+      # Hello
+      hi !
+
+      This is post `id != public_id`!
+    EOS
+    public_id: 100_000,
+    thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
+    published_at: Time.current,
+    category: site1.categories.find_by!(slug: "category2"),
+  )
+
+  50.times do |i|
+    i += 100
+
+    site1.posts.create!(
+      title: "Post #{i}",
+      body: <<~EOS,
+        page 1
+
+        <!--nextpage-->
+
+        page 2
+
+        <!--nextpage-->
+
+        page 3
+      EOS
+      thumbnail: open(Rails.root.join("db/data/thumbnail.jpg")),
+      published_at: Time.current,
+      category: site1.categories.find_by!(slug: "category2"),
     )
   end
 end
