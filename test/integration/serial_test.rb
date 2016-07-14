5739de09e963cd52aea78a46a23906a1de638857
Kenji Okimoto <okimoto@clear-code.com>
2016-07-14 13:44:33 +0900

    Cosmetic

--- test/integration/serial_test.rb
+++ test/integration/serial_test.rb
@@ -11,7 +11,7 @@ class SerialTest < ActionDispatch::IntegrationTest
 
   sub_test_case "/serials" do
     test "page title" do
-      visit "/serials"
+      visit("/serials")
 
       assert_equal("すべての連載 | #{@site.name}", title)
     end
@@ -19,7 +19,7 @@ class SerialTest < ActionDispatch::IntegrationTest
     test "page title with pagination" do
       create_list(:serial, 50, :with_posts, site: @site)
 
-      visit "/serials?page=2"
+      visit("/serials?page=2")
 
       assert_equal("すべての連載 (26〜50/#{Serial.published.count}件) | #{@site.name}", title)
     end
@@ -27,62 +27,62 @@ class SerialTest < ActionDispatch::IntegrationTest
     test "serial titles" do
       visit "/serials"
 
-      serial_titles = find_all ".serial-summary__title"
+      serial_titles = find_all(".serial-summary__title")
       assert_equal(@serials.map(&:title).reverse, serial_titles.map(&:text))
     end
 
     test "number of posts" do
-      visit "/serials"
+      visit("/serials")
 
-      number_of_posts = find_all ".number-of-posts"
+      number_of_posts = find_all(".number-of-posts")
       assert_equal(["記事数：1", "記事数：1", "記事数：1"], number_of_posts.map(&:text))
     end
 
     test "markdown" do
       create(:serial, :with_posts, site: @site, description: <<~DESC)
-      # h1
+        # This is title
 
-      contents
+        contents
       DESC
-      visit "/serials"
+      visit("/serials")
 
-      description = find_all(".serial-summary__content").first
-      assert_equal("h1 contents", description.text)
+      description = first(".serial-summary__content")
+      assert_equal("This is title contents", description.text)
     end
   end
 
   sub_test_case "/serials/:id" do
     test "page title" do
-      visit "/serials"
+      visit("/serials")
 
-      click_on @serials.last.title
+      click_on(@serials.last.title)
 
       assert_equal("#{@serials.last.title} | #{@site.name}", title)
     end
 
     test "serial title" do
-      visit "/serials"
+      visit("/serials")
 
-      click_on @serials.last.title
+      click_on(@serials.last.title)
 
-      title = find ".serial-content__title"
+      title = find(".serial-content__title")
       assert_equal(@serials.last.title, title.text)
     end
 
     test "serial description" do
       serial = create(:serial, :with_posts, title: "serial title", site: @site, description: <<~DESC)
-      # h1
+        # This is title
 
-      contents
+        contents
       DESC
-      visit "/serials"
+      visit("/serials")
 
-      click_on serial.title
+      click_on(serial.title)
 
       within ".serial-content__description" do
-        h1 = find "h1"
-        assert_equal("h1", h1.text)
-        contents = find "p"
+        h1 = find("h1")
+        assert_equal("This is title", h1.text)
+        contents = find("p")
         assert_equal("contents", contents.text)
       end
     end
