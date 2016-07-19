9b4c8c25617591c300248e85359badefe055c582
Kenji Okimoto <okimoto@clear-code.com>
2016-07-19 14:23:58 +0900

    Select multiple categories

--- app/validators/post_category_validator.rb
+++ app/validators/post_category_validator.rb
@@ -1,6 +1,6 @@
 class PostCategoryValidator < ActiveModel::Validator
   def validate(record)
-    if record.category.has_children?
+    if record.categories.all?(&:has_children?)
       record.errors[:category_id] << "子カテゴリを持つカテゴリ「#{record.category.full_name}」に記事を登録することはできません。"
     end
   end
