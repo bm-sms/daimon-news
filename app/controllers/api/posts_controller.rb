1e3a0c1d5210a09600937a92901004c5afbe5329
Kenji Okimoto <okimoto@clear-code.com>
2016-07-21 13:48:17 +0900

    Use categorized scope
    But this action does not work well...

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,8 +1,8 @@
 class Api::PostsController < Api::ApplicationController
   def index
     scope = current_site.posts
-    scope = scope.joins(:category).merge(Category.where(slug: params[:filter][:category_slug])) if params.dig(:filter, :category_slug)
-    scope = scope.where(serial_id: params[:filter][:serial_id])                                 if params.dig(:filter, :serial_id)
+    scope = scope.categorized(Category.find_by!(slug: params[:filter][:category_slug])) if params.dig(:filter, :category_slug)
+    scope = scope.where(serial_id: params[:filter][:serial_id]) if params.dig(:filter, :serial_id)
 
     posts = scope.published.order_by_recent.page(params.dig(:page, :number)).per(params.dig(:page, :size))
     render json: posts, include: "category", meta: pagination_meta(posts)
