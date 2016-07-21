1271751bd00c7aebfabc9885ff24a06a0340f42a
Kenji Okimoto <okimoto@clear-code.com>
2016-07-21 14:06:55 +0900

    Use categories

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,6 @@
 class FeedController < ApplicationController
   def show
-    @posts = current_site.posts.published.order_by_recent.preload(:category).limit(20)
+    @posts = current_site.posts.published.order_by_recent.preload(:categories).limit(20)
 
     render formats: :xml
   end
