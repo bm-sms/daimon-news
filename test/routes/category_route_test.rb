e19abce2915d3d16afadcf467d2d8a1193453ea1
Kenji Okimoto <okimoto@clear-code.com>
2016-07-21 16:09:44 +0900

    Add parentheses

--- test/routes/category_route_test.rb
+++ test/routes/category_route_test.rb
@@ -3,16 +3,16 @@ class CategoryRouteTest < ActionDispatch::IntegrationTest
     @site = create(:site)
     @category = create(:category, site: @site)
     https!
-    host! @site.fqdn
+    host!(@site.fqdn)
   end
 
   def test_first_page
-    get "/category/#{@category.slug}"
+    get("/category/#{@category.slug}")
     assert_response(:success)
   end
 
   def test_redirect_to_2nd_page
-    get "/category/#{@category.slug}/page/2"
+    get("/category/#{@category.slug}/page/2")
     assert_response(:moved_permanently)
     assert_redirected_to("/category/#{@category.slug}?page=2")
   end
