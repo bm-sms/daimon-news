c90652d2fc6819b2beef5d5b0d6a2eff2ce67dcd
Kenji Okimoto <okimoto@clear-code.com>
2016-07-21 16:09:44 +0900

    Add parentheses

--- test/routes/post_routes_test.rb
+++ test/routes/post_routes_test.rb
@@ -4,16 +4,16 @@ class PostRouteTest < ActionDispatch::IntegrationTest
     @category = create(:category, site: @site)
     @post = create(:post, :with_pages, site: @site, categories: [@category])
     https!
-    host! @site.fqdn
+    host!(@site.fqdn)
   end
 
   def test_first_page
-    get "/#{@post.public_id}"
+    get("/#{@post.public_id}")
     assert_response(:success)
   end
 
   def test_redirect_to_2nd_page
-    get "/#{@post.public_id}/2"
+    get("/#{@post.public_id}/2")
     assert_response(:moved_permanently)
     assert_redirected_to("/#{@post.public_id}?page=2")
   end
