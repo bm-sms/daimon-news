506378de5eac0a059a5ed892ec36d00cbeae019b
Kenji Okimoto <okimoto@clear-code.com>
2016-07-21 16:15:15 +0900

    Use categories

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -5,12 +5,12 @@ class Api::PostsController < Api::ApplicationController
     scope = scope.where(serial_id: params[:filter][:serial_id]) if params.dig(:filter, :serial_id)
 
     posts = scope.published.order_by_recent.page(params.dig(:page, :number)).per(params.dig(:page, :size))
-    render json: posts, include: "category", meta: pagination_meta(posts)
+    render json: posts, include: "categories", meta: pagination_meta(posts)
   end
 
   def show
     post = current_site.posts.published.find_by!(public_id: params[:public_id])
 
-    render json: post, include: "related_posts.category, category, serial"
+    render json: post, include: "related_posts.categories, categories, serial"
   end
 end
