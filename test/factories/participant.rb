f468a00bf8e60cac3647e9be036d7fba5e2e2e09
cafedomancer <cafedomancer@gmail.com>
2016-07-27 11:19:43 +0900

    Improve the tests for author pages

--- test/factories/participant.rb
+++ test/factories/participant.rb
@@ -11,5 +11,12 @@ FactoryGirl.define do
     trait :with_photo do
       photo { Rails.root.join("test/fixtures/images/face.png").open }
     end
+
+    trait :with_posts do
+      after :create do |participant, _evaluator|
+        post = create(:post, :whatever, site: participant.site)
+        post.credits << build(:credit, :whatever, site: participant.site, participant: participant)
+      end
+    end
   end
 end
--- test/integration/participant_test.rb
+++ test/integration/participant_test.rb
@@ -3,23 +3,83 @@ require "test_helper"
 class ParticipantTest < ActionDispatch::IntegrationTest
   setup do
     @site = create(:site)
-    @participant = create(:participant, :with_photo, site: @site)
+    @participants = create_list(:participant, 3, :with_photo, :with_posts, site: @site)
 
     switch_domain(@site.fqdn)
   end
 
-  test "should have the name, photo, description and written posts of a participant" do
-    visit("/participants/#{@participant.id}")
+  sub_test_case "/participants" do
+    test "page title" do
+      visit("/participants")
 
-    within ".participant__header" do
-      assert page.has_content?("Author")
-      assert_equal("face.png", File.basename(find("img").native["src"]))
+      assert_equal("すべての執筆関係者 | #{@site.name}", title)
     end
 
-    within ".participant__body" do
-      assert page.has_content?("Awesome description")
+    test "page title with pagination" do
+      create_list(:participant, 50, :with_posts, site: @site)
+
+      visit("/participants?page=2")
+
+      assert_equal("すべての執筆関係者 (26〜50/#{Participant.published.count}件) | #{@site.name}", title)
     end
 
-    # TODO: check whether the posts written by a participant exists
+    test "participant titles" do
+      visit "/participants"
+
+      participant_titles = find_all(".participant-summary__title")
+      assert_equal(@participants.map(&:name).reverse, participant_titles.map(&:text))
+    end
+
+    test "number of posts" do
+      visit("/participants")
+
+      number_of_posts = find_all(".number-of-posts")
+      assert_equal(["記事数：1", "記事数：1", "記事数：1"], number_of_posts.map(&:text))
+    end
+
+    test "markdown" do
+      create(:participant, :with_posts, site: @site, summary: "This is a summary")
+      visit("/participants")
+
+      description = first(".participant-summary__content")
+      assert_equal("This is a summary", description.text)
+    end
+  end
+
+  sub_test_case "/participants/:id" do
+    test "page title" do
+      visit("/participants")
+
+      click_on(@participants.last.name)
+
+      assert_equal("#{@participants.last.name} | #{@site.name}", title)
+    end
+
+    test "participant title" do
+      visit("/participants")
+
+      click_on(@participants.last.name)
+
+      title = find(".participant-content__title")
+      assert_equal(@participants.last.name, title.text)
+    end
+
+    test "participant description" do
+      participant = create(:participant, :with_posts, name: "participant name", site: @site, description: <<~DESC)
+        # This is title
+
+        contents
+      DESC
+      visit("/participants")
+
+      click_on(participant.name)
+
+      within ".participant-content__description" do
+        h1 = find("h1")
+        assert_equal("This is title", h1.text)
+        contents = find("p")
+        assert_equal("contents", contents.text)
+      end
+    end
   end
 end
