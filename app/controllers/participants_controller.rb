bfaf0a68818da40fead692cbd5d60a6fa369a2c2
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-07-28 20:22:44 +0900

    Raise an error if participants page is not allowed

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,4 +1,9 @@
 class ParticipantsController < ApplicationController
+  before_action :require_view_participants
+
+  class NotAllowed < StandardError
+  end
+
   def index
     @participants = current_site.participants.having_published_posts.sorted.page(params[:page])
     @participants.extend(ParticipantsDecorator)
@@ -9,5 +14,11 @@ class ParticipantsController < ApplicationController
     @posts = @participant.posts.published.eager_load(credits: :role).order_by_recent.page(params[:page])
 
     @posts.extend(PaginationInfoDecorator)
+  end
+
+  private
+
+  def require_view_participants
+    raise NotAllowed unless current_site.view_participants?
   end
 end
