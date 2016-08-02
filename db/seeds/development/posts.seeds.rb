0a48afff8ab7831981fa4b91dbb59ac5b14554a2
Ryunosuke Sato <tricknotes.rs@gmail.com>
2016-08-02 17:32:12 +0900

    Update seed to create post with category in atomically

--- db/seeds/development/posts.seeds.rb
+++ db/seeds/development/posts.seeds.rb
@@ -12,8 +12,8 @@ after "development:sites", "development:categories" do
     EOS
     thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
     published_at: Time.current,
+    categorizations_attributes: [{category: category1, order: 1}],
   )
-  post.categorizations.create!(category: category1, order: 1)
 
   published_at = 3.minutes.from_now
 
@@ -27,8 +27,8 @@ after "development:sites", "development:categories" do
     EOS
     thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
     published_at: published_at,
+    categorizations_attributes: [{category: category2, order: 1}],
   )
-  post.categorizations.create!(category: category2, order: 1)
 
   paginate_body = <<~EOS
     page 1
@@ -50,9 +50,8 @@ after "development:sites", "development:categories" do
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
-      categories: [],
+      categorizations_attributes: [{category: category2, order: 1}],
     )
-    post.categorizations.create!(category: category2, order: 1)
   end
 
   # below records: id != public_id
@@ -67,8 +66,8 @@ after "development:sites", "development:categories" do
     public_id: 100_000,
     thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
     published_at: Time.current,
+    categorizations_attributes: [{category: category2, order: 1}],
   )
-  post.categorizations.create!(category: category2, order: 1)
 
   50.times do |i|
     i += 100
@@ -78,8 +77,8 @@ after "development:sites", "development:categories" do
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [{category: category2, order: 1}],
     )
-    post.categorizations.create!(category: category2, order: 1)
   end
 
   25.times do |i|
@@ -89,40 +88,42 @@ after "development:sites", "development:categories" do
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [{category: site1.categories.find_by!(slug: "category3_1_1"), order: 1}],
     )
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_1_1"), order: 1)
     i += 1
     post = site1.posts.create!(
       title: "Post #{i}",
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [{category: site1.categories.find_by!(slug: "category3_1_2"), order: 1}],
     )
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_1_2"), order: 1)
     i += 1
     post = site1.posts.create!(
       title: "Post #{i}",
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [
+        {category: site1.categories.find_by!(slug: "category3_1_3"), order: 1},
+        {category: site1.categories.find_by!(slug: "category3_1_4"), order: 2},
+      ],
     )
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_1_3"), order: 1)
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_1_4"), order: 2)
     i += 1
     post = site1.posts.create!(
       title: "Post #{i}",
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [{category: site1.categories.find_by!(slug: "category3_2"), order: 1}],
     )
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_2"), order: 1)
     i += 1
     post = site1.posts.create!(
       title: "Post #{i}",
       body: paginate_body,
       thumbnail: Rails.root.join("db/data/thumbnail.jpg").open,
       published_at: Time.current,
+      categorizations_attributes: [{category: site1.categories.find_by!(slug: "category3_3"), order: 1}],
     )
-    post.categorizations.create!(category: site1.categories.find_by!(slug: "category3_3"), order: 1)
   end
 end
