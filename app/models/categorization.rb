1726cb5debc5d0fae9726820e768a469c6dcbabf
Kenji Okimoto <okimoto@clear-code.com>
2016-08-04 11:50:02 +0900

    Use Post custom validation instead of Categorization's validation
    Because in previous version, we cannot validate multiple same categories
    when create/update Post.

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -2,7 +2,7 @@ class Categorization < ActiveRecord::Base
   belongs_to :category
   belongs_to :post
 
-  validates :category_id, presence: true, uniqueness: {scope: :post_id}
+  validates :category_id, presence: true
   validates :order, numericality: :only_integer
 
   scope :ordered, -> { order(:order) }
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -64,10 +64,18 @@ class PostTest < ActiveSupport::TestCase
       @child_category = create(:category, site: @site, parent: @parent_category)
     end
 
-    test "validate_with PostCategoryValidator" do
-      post = build(:post, categories: [@parent_category], site: @site)
-      assert { !post.valid? }
-      assert_equal(["子カテゴリを持つカテゴリ「#{@parent_category.full_name}」に記事を登録することはできません。"], post.errors[:categorizations])
+    sub_test_case "validate_with PostCategoryValidator" do
+      test "category has children" do
+        post = build(:post, categories: [@parent_category], site: @site)
+        assert { !post.valid? }
+        assert_equal(["子カテゴリを持つカテゴリ「#{@parent_category.full_name}」に記事を登録することはできません。"], post.errors[:categorizations])
+      end
+
+      test "same category twice" do
+        post = build(:post, categorizations_attributes: [{category: @child_category, order: 1}, {category: @child_category, order: 2}], site: @site)
+        assert { !post.valid? }
+        assert_equal(["同一カテゴリを複数選択することはできません。"], post.errors[:categorizations])
+      end
     end
   end
 end
