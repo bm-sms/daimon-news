50a4708cf653669a92cd2dc3d442b5c18115398b
Kenji Okimoto <okimoto@clear-code.com>
2016-08-04 11:36:27 +0900

    Use meaningful name

--- app/validators/post_category_validator.rb
+++ app/validators/post_category_validator.rb
@@ -1,7 +1,7 @@
 class PostCategoryValidator < ActiveModel::Validator
   def validate(record)
-    record.categorizations.each do |c|
-      category = Category.find(c.category_id)
+    record.categorizations.each do |categorization|
+      category = Category.find(categorization.category_id)
       if category.has_children?
         record.errors[:categorizations] << "子カテゴリを持つカテゴリ「#{category.full_name}」に記事を登録することはできません。"
       end
