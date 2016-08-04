64b0115e359233899fda518616060710ef0539a4
Kenji Okimoto <okimoto@clear-code.com>
2016-08-04 11:21:31 +0900

    Fix custom validation
    In previous version, we can register post with category has children.

--- app/validators/post_category_validator.rb
+++ app/validators/post_category_validator.rb
@@ -1,6 +1,7 @@
 class PostCategoryValidator < ActiveModel::Validator
   def validate(record)
-    record.categories.each do |category|
+    record.categorizations.each do |c|
+      category = Category.find(c.category_id)
       if category.has_children?
         record.errors[:categorizations] << "子カテゴリを持つカテゴリ「#{category.full_name}」に記事を登録することはできません。"
       end
