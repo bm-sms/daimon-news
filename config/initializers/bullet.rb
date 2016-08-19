433906c950349b857a4de0c0deee390f9c037872
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2016-08-19 11:07:41 +0900

    Hello bullet

---
+++ config/initializers/bullet.rb
@@ -0,0 +1,8 @@
+Rails.application.config.after_initialize do
+  Bullet.enable = true
+  Bullet.alert = true
+  Bullet.bullet_logger = true
+  Bullet.console = true
+  Bullet.rails_logger = true
+  Bullet.add_footer = true
+end if Rails.env.development?
