4e0221bfd98d316622c6bd37eb4ab8dd35fc2e17
xmeta <xmeta@users.noreply.github.com>
2016-10-11 13:14:16 +0900

    TEST(RedirectRule): add tests

---
+++ test/routes/redirect_routes_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class RedirectRoutesTest < ActionDispatch::IntegrationTest
+  def setup
+    @site = create(:site, fqdn: "127.0.0.1")
+    @category = create(:category, site: @site)
+    @post = create(:post, :with_pages, site: @site, categorizations_attributes: [{category: @category, order: 1}])
+    @redirect_rule = RedirectRule.create!(site: @site, request_path: "/page1", destination: "/#{@post.public_id}?page=1")
+    https!
+    host!(@site.fqdn)
+  end
+
+  def test_redirect
+    get("/page1")
+    assert_response(:moved_permanently)
+    assert_redirected_to("/#{@post.public_id}?page=1")
+  end
+end
