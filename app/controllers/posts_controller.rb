c23834c9789b14a67164bec87029db7abceee806
Atsushi Fukuda <atsushi-fukuda@bm-sms.co.jp>
2016-10-14 13:23:57 +0900

    Fixing issue #627

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   helper_method :current_category
+  class PageNotFound < StandardError
+  end
 
   def show
     @post = current_site.posts.published.find_by!(public_id: params[:public_id])
@@ -10,6 +12,7 @@ class PostsController < ApplicationController
       else
         @post.pages.page(params[:page]).per(1)
       end
+    raise PostsController::PageNotFound if @pages.blank?
   end
 
   private
