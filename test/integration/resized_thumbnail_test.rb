3a4cdf6170f4019f6256164e231e8adef3faa32a
naofumi-fujii <naofumi.fujii@gmail.com>
2016-12-20 12:43:27 +0900

    fix failing test

--- test/integration/resized_thumbnail_test.rb
+++ test/integration/resized_thumbnail_test.rb
@@ -1,22 +1,37 @@
 require 'test_helper'
 
 class ResizedThumbnailTest < ActionDispatch::IntegrationTest
-  setup do
-    @post = create(
-      :post,
-      :whatever,
-      title: "Hi"
-    )
-    switch_domain(@post.site.fqdn)
+  sub_test_case "not using resized thumbnail" do
+    setup do
+      post_resize_thumb = create(
+        :post,
+        :whatever,
+        title: "Hi"
+      )
+      switch_domain(post_resize_thumb.site.fqdn)
+    end
+
+    test "not using resized thumbnail" do
+      visit "/"
+      assert_equal(first(".article-summary__thumbnail")["src"], "/uploads/post/thumbnail/1/thumbnail.jpg")
+    end
   end
 
-  test "not using resized thumbnail" do
-    visit "/"
-    assert_equal(first(".article-summary__thumbnail")["src"], "/uploads/post/thumbnail/1/thumbnail.jpg")
-  end
+  sub_test_case "using resized thumbnail" do
+    setup do
+      site = create(:site, :resize_thumb)
+      post_resize_thumb = create(
+        :post,
+        :whatever,
+        site: site,
+        title: "Hi"
+      )
+      switch_domain(post_resize_thumb.site.fqdn)
+    end
 
-  test "using resized thumbnail" do
-    visit "/"
-    assert_equal(first(".article-summary__thumbnail")["src"], "/uploads/post/thumbnail/2/resized_thumbnail.jpg")
+    test "using resized thumbnail" do
+      visit "/"
+      assert_equal(first(".article-summary__thumbnail")["src"], "/uploads/post/thumbnail/2/resized_thumbnail.jpg")
+    end
   end
 end
