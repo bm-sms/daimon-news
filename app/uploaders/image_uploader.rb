d51d4bb22b2abc0ac89c8c4efa24d9b375719658
naofumi-fujii <naofumi.fujii@gmail.com>
2016-12-21 14:24:12 +0900

    create resize version only when image is resizable

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,15 @@
 class ImageUploader < ApplicationUploader
   include CarrierWave::MiniMagick
 
+  version :resized, if: :resizable?
+
   version :resized do
     process resize_to_fill: [100, 100]
+  end
+
+  private
+
+  def resizable? picture
+    !picture.content_type.blank?
   end
 end
